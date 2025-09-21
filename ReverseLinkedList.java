ea664df46ec367ceb91565df7f621109f638fa63
utkarsh55 <utkarsh4567sharma@gmail.com>
2025-09-21 08:42:43 +0530

    More Codes

---
+++ ReverseLinkedList.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    // Iterative method
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next; // store next
+            curr.next = prev;              // reverse link
+            prev = curr;                   // move prev
+            curr = nextNode;               // move curr
+        }
+        return prev; // new head
+    }
+
+    // Helper to print list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Creating Linked List: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("Reversed List: ");
+        printList(head);
+    }
+}
+
