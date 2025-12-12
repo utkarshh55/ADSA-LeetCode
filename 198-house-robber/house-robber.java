9cf048534ae4dfb721406f4472398cfdec162a0a
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2025-12-13 02:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (48.73%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    private int[] dp;
+
+    private int maxLoot(int[] nums, int idx) {
+        if (idx >= nums.length) return 0;
+        if (dp[idx] != -1) return dp[idx];
+
+        int steal = nums[idx] + maxLoot(nums, idx + 2);
+        int skip = maxLoot(nums, idx + 1);
+
+        return dp[idx] = Math.max(steal, skip);
+    }
+
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return maxLoot(nums, 0);
+    }
+}
