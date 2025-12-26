28add5ef6ff4673c5480e2144902e9c32e49eccd
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2025-12-26 11:13:05 +0530

    Time: 11 ms (75.86%) | Memory: 46.9 MB (48.79%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,21 +1,21 @@
 class Solution {
     public int bestClosingTime(String customers) {
-        int penalty=0;
-        for(char c: customers.toCharArray()){
-            if(c=='N') penalty++;
+        int penalty = 0;
+        for (char c : customers.toCharArray()) {
+            if (c == 'N') penalty++;
         }
-        int minPenalty=penalty;
-        int bestHour=0;
-        for(int i=0;i<customers.length();i++){
-            char c=customers.charAt(i);
-            if(c=='Y'){
-                penalty--;
-            }else{
-                penalty++;
+        int minPenalty = penalty;
+        int bestHour = 0;
+        for (int i = 0; i < customers.length(); i++) {
+            char c = customers.charAt(i);
+            if (c == 'Y') {
+                penalty--; 
+            } else {
+                penalty++; 
             }
-            if(penalty<minPenalty){
-                minPenalty=penalty;
-                bestHour=i+1;
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestHour = i + 1;
             }
         }
         return bestHour;
