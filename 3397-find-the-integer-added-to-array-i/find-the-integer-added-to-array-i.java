3969936aaf4adb0d70e699bb76bb091d2488095c
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-01-01 13:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (94.84%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int min1=nums1[0];
+        int min2=nums2[0];
+        for(int num:nums1){
+            min1=Math.min(min1,num);
+        }
+        for(int num:nums2){
+            min2=Math.min(min2,num);
+        }
+        return min2-min1;
+    }
+}
