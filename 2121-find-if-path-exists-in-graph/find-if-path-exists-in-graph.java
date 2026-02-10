4439a2f2b62e682d95542c73a92340ba4cf31bdb
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-02-10 22:47:16 +0530

    Time: 132 ms (50.06%) | Memory: 309.5 MB (11.75%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,31 +1,29 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int src, int destination) {
-        if(src==destination) return true;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+    public boolean dfs(int node, int destination,ArrayList<ArrayList<Integer>> adj,boolean[] visited){
+        if(node==destination) return true;
+        visited[node]=true;
+        for(int i=0;i<adj.get(node).size();i++){
+            int neigh=adj.get(node).get(i);
+            if(!visited[neigh]){
+                if(dfs(neigh,destination,adj,visited)){
+                    return true;
+                }
+            }
         }
+        return false;
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(source==destination) return true;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
         for(int i=0;i<edges.length;i++){
             int from=edges[i][0];
             int to=edges[i][1];
             adj.get(from).add(to);
             adj.get(to).add(from);
         }
-        Queue<Integer> q=new LinkedList<>();
-        boolean [] visited=new boolean[n];
-        q.add(src);
-        visited[src]=true;
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            for(int i=0;i<adj.get(curr).size();i++){
-                int neigh=adj.get(curr).get(i);
-                if(neigh==destination) return true;
-                if(!visited[neigh]){
-                    visited[neigh]=true;
-                    q.add(neigh);
-                }
-            }
-        }
-        return false;
+        boolean[] visited=new boolean[n];
+        return dfs(source,destination,adj,visited);
+
     }
 }
