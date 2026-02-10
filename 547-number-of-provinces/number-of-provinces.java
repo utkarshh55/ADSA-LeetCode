46aa6271b429fc2d0eb85c3c0de4ef5284f07a17
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-02-10 10:41:35 +0530

    Time: 3 ms (32.97%) | Memory: 50.1 MB (7.20%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,28 +1,40 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int provinces = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(isConnected, visited, i);
-                provinces++;
+    public void bfs(int start,boolean visited[],ArrayList<ArrayList<Integer>> adj){
+        Queue<Integer> q=new LinkedList<>();
+            q.add(start);
+            visited[start]=true;
+            while(!q.isEmpty()){
+                int curr=q.poll();
+                for(int i=0;i<adj.get(curr).size();i++){
+                    int neigh=adj.get(curr).get(i);
+                    if(visited[neigh]==false){
+                        visited[neigh]=true;
+                        q.add(neigh);
+                    }
+                }
             }
-        }
-        return provinces;
     }
-    private void bfs(int[][] graph, boolean[] visited, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        visited[start] = true;
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            for(int j=0;j<graph.length;j++){
-                if(graph[curr][j]==1 && !visited[j]){
-                    visited[j]=true;
-                    q.add(j);
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=isConnected.length;
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if (isConnected[i][j] == 1 && i != j) {
+                int from=i;
+                int to=j;
+                adj.get(from).add(to);
                 }
             }
         }
+        int count=0;
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<visited.length;i++){
+            if(visited[i]==false){
+                count++;
+                bfs(i,visited,adj);
+            }
+        }
+        return count;
     }
 }
