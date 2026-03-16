0d9cbf8dceb20f479997f20ff6676e10386a2432
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-03-16 15:19:41 +0530

    Time: 9 ms (25.46%) | Memory: 51 MB (5.77%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,17 +1,38 @@
 class NumArray {
-    int arr[];
+    int[] tree;
+    int n;
+
     public NumArray(int[] nums) {
-        arr=nums;
+        n=nums.length;
+        tree=new int[4*n];
+        build(0,0,n-1,nums);
     }
-    public int sumRange(int left, int right) {
-        int sum=0;
-        for(int i=left;i<=right;i++){
-            sum+=arr[i];
+    void build(int node,int start,int end,int[]nums){
+        if(start==end){
+            tree[node]=nums[start];
+            return;
+
         }
-        return sum;
+        int mid=(start+end)/2;
+        build(2*node+1,start,mid,nums);
+        build(2*node+2,mid+1,end,nums);
+        tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+    }
+    
+    public int sumRange(int left, int right) {
+        return query(0,0,n-1,left,right);
+
+        
+    }
+    public int query(int node,int start,int end,int l,int r){
+        if(r<start || end<l)return 0;
+        if(l<=start && end<=r){
+            return tree[node];
+        }
+        int mid=(start+end)/2;
+        return query(2*node+1,start,mid,l,r)+query(2*node+2,mid+1,end,l,r);
     }
 }
-
 
 /**
  * Your NumArray object will be instantiated and called as such:
