7be484e4b1805e2c0ebb23ce8249895023270e46
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-04-06 20:51:29 +0530

    Time: 36 ms (37.68%) | Memory: 51.9 MB (72.83%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -0,0 +1,35 @@
+//Copy paste
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        for (int[] o : obstacles) {
+            set.add(o[0] + "#" + o[1]);
+        }
+        int x = 0, y = 0;
+        int dir = 0; // 0=N, 1=E, 2=S, 3=W
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        int maxDist = 0;
+        for (int cmd : commands) {
+            if (cmd == -1) {
+                dir = (dir + 1) % 4; // turn right
+            } 
+            else if (cmd == -2) {
+                dir = (dir + 3) % 4; // turn left
+            } 
+            else {
+                for (int i = 0; i < cmd; i++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+                    if (set.contains(nx + "#" + ny)) {
+                        break; // obstacle found
+                    }
+                    x = nx;
+                    y = ny;
+                    maxDist = Math.max(maxDist, x * x + y * y);
+                }
+            }
+        }
+        return maxDist;
+    }
+}
