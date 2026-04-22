0d277cf7a01df4964a610a22eaa8f4924386029d
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-04-22 14:59:50 +0530

    Time: 2 ms (90.36%) | Memory: 44.1 MB (58.49%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,52 +1,45 @@
 class Solution {
-    private int[] delRow={-1,0,1,0};
-    private int[] delCol={0,1,0,-1};
-    public boolean isValid(int i,int j,int m,int n){
-        if((i>=0 && i<m) &&(j>=0 && j<n)){
-            return true;
-        }
-        return false;
+    int[] delRow = {1, 0, -1, 0};
+    int[] delCol = {0, 1, 0, -1};
+    public boolean isValid(int i, int j, int m, int n) {
+        return (i >= 0 && i < m && j >= 0 && j < n);
     }
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][]vis=new int[m][n];
-        int time=0;
-        int totalOranges=0;
-        int cnt=0;
-        Queue<int[]>q= new LinkedList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int cell=grid[i][j];
-                if(cell==2){
-                    q.offer(new int[]{i,j});     
+        int m = grid.length;
+        int n = grid[0].length;
+        int time = 0;
+        int fresh = 0;
+        int rotted = 0;
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
                 }
-                if(cell!=0) totalOranges++;
             }
         }
-        while(!q.isEmpty()){
-            int k=q.size();
-            cnt+=k;
-            while(k-->0){
-                int []cell=q.poll();
-                int row=cell[0];
-                int col=cell[1];
-                for(int i=0;i<4;i++){
-                    int nRow=row+delRow[i];
-                    int nCol=col+delCol[i];
-                    if(isValid(nRow,nCol,m,n) && grid[nRow][nCol]==1){
-                        grid[nRow][nCol]=2;
-                        q.offer(new int[]{nRow,nCol});
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean spread = false;
+            for (int k = 0; k < size; k++) {
+                int[] cell = q.poll();
+                int row = cell[0];
+                int col = cell[1];
+                for (int i = 0; i < 4; i++) {
+                    int newRow = row + delRow[i];
+                    int newCol = col + delCol[i];
+                    if (isValid(newRow, newCol, m, n) && grid[newRow][newCol] == 1) {
+                        grid[newRow][newCol] = 2;
+                        q.add(new int[]{newRow, newCol});
+                        rotted++;
+                        spread = true;
                     }
-
                 }
             }
-            if(!q.isEmpty()) time++;
+            if (spread) time++;
         }
-
-
-        if(totalOranges==cnt) return time;
-        return -1;
-        
+        return (rotted == fresh) ? time : -1;
     }
 }
