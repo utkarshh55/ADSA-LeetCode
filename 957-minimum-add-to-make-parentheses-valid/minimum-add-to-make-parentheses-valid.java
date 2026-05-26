def75bd87391aed0d63fa197431957ea08c34210
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-05-26 14:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (21.11%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int add = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                open++;
+            } else {
+                if(open > 0) {
+                    open--;
+                } else {
+                    add++;
+                }
+            }
+        }
+        return add + open;
+    }
+}
