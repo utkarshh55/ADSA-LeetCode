ea0b387904c79ea5670c1555e066bcb36c8b4e2c
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-06-01 11:21:00 +0530

    Time: 1 ms (99.89%) | Memory: 46.3 MB (22.74%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int solve(int row, int col, List<List<Integer>> triangle, Integer[][] dp) {
+    public int solve(int row,int col,List<List<Integer>> triangle,Integer[][]dp){
         int n = triangle.size();       
         if (row == n - 1) return triangle.get(row).get(col);
         if (dp[row][col] != null) return dp[row][col];
         int left = solve(row + 1, col, triangle, dp);
-        int right = solve(row + 1, col + 1, triangle, dp);
-        return dp[row][col] = triangle.get(row).get(col) + Math.min(left, right);
+        int right= solve(row+1,col+1,triangle,dp);
+        return dp[row][col]= triangle.get(row).get(col)+Math.min(left,right);
     }
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
