f9c0a574a16ec6f2cd5a1dfcb67bc7a2c591e3ec
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-06-05 09:49:11 +0530

    Time: 8 ms (95.96%) | Memory: 49.2 MB (55.71%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -15,23 +15,23 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        int level = 1;
-        int maxLevel = 1;
-        int maxSum = Integer.MIN_VALUE;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int sum = 0;
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                sum += node.val;
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int level=1;
+        int maxLevel=1;
+        int maxSum=Integer.MIN_VALUE;
+        while(!q.isEmpty()){
+            int size=q.size();
+            int sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                sum+=curr.val;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
             }
-            if (sum > maxSum) {
-                maxSum = sum;
-                maxLevel = level;
+            if(sum>maxSum){
+                maxSum=sum;
+                maxLevel=level;
             }
             level++;
         }
