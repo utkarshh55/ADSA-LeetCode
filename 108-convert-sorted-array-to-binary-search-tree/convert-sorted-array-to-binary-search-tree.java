89e3ba64ec0751864940274c792d361a0e9f8b16
Utkarsh Sharma <utkarsh4567sharma@gmail.com>
2026-06-08 14:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (25.47%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -14,15 +14,15 @@
  * }
  */
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return builtBST(nums,0,nums.length-1);
-    }
-    public TreeNode builtBST(int[]nums, int left,int right){
+    public TreeNode helper(int[] nums,int left,int right){
         if(left>right) return null;
         int mid=left+(right-left)/2;
         TreeNode root=new TreeNode(nums[mid]);
-        root.left=builtBST(nums,left,mid-1);
-        root.right=builtBST(nums,mid+1,right);
+        root.left=helper(nums,left,mid-1);
+        root.right=helper(nums,mid+1,right);
         return root;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
     }
 }
